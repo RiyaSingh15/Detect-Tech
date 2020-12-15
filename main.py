39b3ea58777a8ca257e1226ea292afd536261bf7
Riya <r.riyasingh.s15@gmail.com>
2020-12-15 19:59:53 +0530

    Gender and age detecting

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from keras.preprocessing.image import ImageDataGenerator
 
 class Detect:
     def __init__(self):
+        self.gen = None
+        self.der = None
         self.emotion_model = Sequential()
         self.emotion_model.add(Conv2D(32, kernel_size=(
             3, 3), activation='relu', input_shape=(48, 48, 1)))
@@ -46,6 +48,7 @@ class Detect:
     def gui(self):
         self.inputVideo()
         self.outputVideo()
+        self.outputGender()
         self.root.resizable(0, 0)
         self.root.title("Detect Tech")
         heading = Label(self.root, text="Detect Tech",
@@ -61,9 +64,9 @@ class Detect:
         emotion_output = Label(
             self.root, text=self.emotion_dict[self.show_text[0]], font=("Arial", 15))
         gender_output = Label(
-            self.root, text="Male/Female", font=("Arial", 15))
+            self.root, text=self.gen, font=("Arial", 15))
         age_output = Label(
-            self.root, text="0-3/4-7/8-14/15-25/25-38/38-48/48-60/60-100", font=("Arial", 15))
+            self.root, text=self.der, font=("Arial", 15))
         iimg = cv2.imread("Input.png")
         oimg = cv2.imread("Output.png")
         width = 290
@@ -117,6 +120,83 @@ class Detect:
         frame1 = cv2.imread(self.emoji_dist[self.show_text[0]])
         pic = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
         cv2.imwrite("Output.png", pic)
+
+    def outputGender(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--image')
+
+        args = parser.parse_args()
+
+        faceProto = "opencv_face_detector.pbtxt"
+        faceModel = "opencv_face_detector_uint8.pb"
+        ageProto = "age_deploy.prototxt"
+        ageModel = "age_net.caffemodel"
+        genderProto = "gender_deploy.prototxt"
+        genderModel = "gender_net.caffemodel"
+
+        MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+        ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
+                   '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+        genderList = ['Male', 'Female']
+
+        faceNet = cv2.dnn.readNet(faceModel, faceProto)
+        ageNet = cv2.dnn.readNet(ageModel, ageProto)
+        genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+        video = cv2.VideoCapture(args.image if args.image else 0)
+        padding = 20
+        i = 0
+        while i < 20:
+            hasFrame, frame = video.read()
+            if not hasFrame:
+                cv2.waitKey()
+                break
+
+            resultImg, faceBoxes = self.highlightFace(faceNet, frame)
+            if not faceBoxes:
+                print("No face detected")
+
+            for faceBox in faceBoxes:
+                face = frame[max(0, faceBox[1]-padding):
+                             min(faceBox[3]+padding, frame.shape[0]-1), max(0, faceBox[0]-padding):min(faceBox[2]+padding, frame.shape[1]-1)]
+
+                blob = cv2.dnn.blobFromImage(
+                    face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+                genderNet.setInput(blob)
+                genderPreds = genderNet.forward()
+                gender = genderList[genderPreds[0].argmax()]
+                self.gen = gender
+
+                ageNet.setInput(blob)
+                agePreds = ageNet.forward()
+                age = ageList[agePreds[0].argmax()]
+                self.der = age
+
+                cv2.putText(resultImg, f'{gender}, {age}', (
+                    faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+            i += 1
+
+    def highlightFace(self, net, frame, conf_threshold=0.7):
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]
+        blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [
+                                     104, 117, 123], True, False)
+
+        net.setInput(blob)
+        detections = net.forward()
+        faceBoxes = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3]*frameWidth)
+                y1 = int(detections[0, 0, i, 4]*frameHeight)
+                x2 = int(detections[0, 0, i, 5]*frameWidth)
+                y2 = int(detections[0, 0, i, 6]*frameHeight)
+                faceBoxes.append([x1, y1, x2, y2])
+                cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2),
+                              (0, 255, 0), int(round(frameHeight/150)), 8)
+        return frameOpencvDnn, faceBoxes
 
 
 if __name__ == "__main__":
